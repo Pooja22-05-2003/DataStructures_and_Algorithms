d0a7bcdd0931e0f84fbcacf6d0fd50bb52dce70d
Pooja <studentpooja1234@gmail.com>
2023-10-23 09:50:41 +0530

    File Updated

---
+++ Topic_Wise_DSA/1-Hashing/9_Largest_subarray_with_0_sum/Sol1.cpp
@@ -0,0 +1,37 @@
+/*
+Using Hashing
+         0, 1, 2, 3,4,5, 6, 7
+Arr[]={ 15,-2, 2,-8,1,7,10,23}
+Prefix={15,13,15,7,8,15,25,48}
+Sum
+
+Unordered_Map store {el->index [First Occurence of that ele]}}
+
+Here 
+if found prefixSum[i]==0 then update the maxLen=max(maxLen,i+1)
+else if (Map.find(PrefixSum[i])!=Map.end()) maxLen=max(maxLen,(i-Map[PrefixSum[i])])
+
+
+*/
+
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        // Your code here
+        unordered_map<int,int> PrefixSum;
+        int maxLen=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=A[i];
+            if(sum==0) maxLen=max(maxLen,i+1);
+            else {
+                if(PrefixSum.find(sum)!=PrefixSum.end()) maxLen=max(maxLen,(i-PrefixSum[sum]));
+                else PrefixSum[sum]=i;
+            }
+        }
+        
+        return maxLen;
+    }
+};
