beacdb3b7293101f1cda343d641f963ec9f36210
Pooja <studentpooja1234@gmail.com>
2023-10-25 11:06:45 +0530

    File Updated

--- Topic_Wise_DSA/13_Dynamic_Programming/2_Maximum_Subset_Sum_Having_No_2_el_adjacent/Sol1.cpp
+++ Topic_Wise_DSA/13_Dynamic_Programming/2_Maximum_Subset_Sum_Having_No_2_el_adjacent/Sol1.cpp
@@ -6,3 +6,64 @@ Approach
 4.Finally return dp[n] as answer.
 
 */
+// TC=O(N)
+// SC=O(N)
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> arr  ){
+    int n=arr.size();
+    int dp[n+1];
+    memset(dp,0,n+1);
+    int ans=0;
+    //  here dp[1]!=arr[1] beacuse negative value are also allowed
+    // So taking 0 is better then taking negative value.
+    dp[1]=max(arr[1],ans);
+    dp[2]=max(arr[2],dp[1]);
+    // dp[3]=max(arr[3]+dp[1],dp[2]);
+
+    //  general formula 
+    // dp[i]=max(arr[i]+dp[i-2],dp[i-1]);
+
+    for(int i=3;i<=n;i++){
+        dp[i]=max((arr[i]+dp[i-2]),dp[i-1]);
+    }
+
+    return dp[n];
+
+
+}
+int main()
+{
+
+    //**********
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    //*********
+    // 1-- based indexing are used here
+
+    int n;
+    cin >> n ;
+    
+    vector<int> arr(n+1,0);
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<solve(arr)<<endl;
+}
+
+/*
+
+5
+1 44 5 -2 8 
+
+
+output :
+52
+
+*/
