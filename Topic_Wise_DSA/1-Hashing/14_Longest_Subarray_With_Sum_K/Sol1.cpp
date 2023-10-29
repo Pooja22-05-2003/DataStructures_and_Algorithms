324ea79079eed97f56cea1054f7d82b9f4019483
Pooja <studentpooja1234@gmail.com>
2023-10-30 00:11:57 +0530

    File Updated

--- Topic_Wise_DSA/1-Hashing/14_Longest_Subarray_With_Sum_K/Sol1.cpp
+++ Topic_Wise_DSA/1-Hashing/14_Longest_Subarray_With_Sum_K/Sol1.cpp
@@ -23,7 +23,14 @@
             }
            
            }
-            
+            //  Remeber to put this if condition , here , because we want the first index
+            /*
+            - See assume aapko sum=15 mila 1st index and 4th index  pe
+            - Toh humme toh longest sub array chaiye na
+            - Toh hum (15,1) ko 15,4 se kyu hi update karenge
+            - Thats why hum phele check kar rhe hai ki if...humara sum phele exist nahi 
+              karta hai tabhi hum map mein daalenge
+            */
             if(PrefixSum.find(sum)==PrefixSum.end())PrefixSum[sum]=i;
 
            
