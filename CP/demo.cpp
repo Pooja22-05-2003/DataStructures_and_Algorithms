14ed4ef8392bd84a2eca782c97ed93fc5dc502d4
Pooja <studentpooja1234@gmail.com>
2023-12-30 16:49:03 +0530

    File Updated

--- CP/demo.cpp
+++ CP/demo.cpp
@@ -1,105 +1,108 @@
-// TC=O(N*q)     N time for each test case
-// SC=O(N)       dp array space complexity
+/*
+Otpmized  approach.
+
+✨✨ Similar to previous question  (just replace sum with d=b[j]-b[i])
+As we are starting from 1 to j1, then again from 2 to j1, we can use two pointer 
+and can use the previous sum in our current answer.
+
+
+*/
+
+// TC=O(n) 
+// SC=O(1)
 
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
 
-ll max(ll a, ll b, ll c) {
-    return max(a, max(b, c));
+int solve(vector<int> &b,int n , int k ){
+    int i = 1 ;
+    int j = 1 ;
+    int ans=0;
+    int d=0;
+    while(i<=n && j<=n){
+        if(i==j){
+             d = b[j] - b[i] ;
+            if(d>k){
+                i++;
+                j++;
+                if(j<=n)  d = b[j] - b[i] ;;
+            }
+            else{
+                int len=1;
+                ans=max(ans,len);
+                j++;
+                if(j<=n){
+                    d = b[j] - b[i] ;;
+                }
+            }
+        }
+        else {
+             d = b[j] - b[i] ;
+            if(d>k){
+                // it means i to j-1 is surely valid
+                ans=max(ans,((j-1)-i+1));
+
+                //  now u have considered i to j-1
+
+                // i-1 to j-1 is surely smaller than equal to k
+                 
+                i++;
+                
+                j++;
+                if(j<=n){
+                     d = b[j] - b[i] ;
+                }
+
+            }
+            else {
+                j++;
+                if(j<=n){
+                     d = b[j] - b[i] ;
+                }
+                
+            }
+        }
+    }
+
+    return ans;
 }
 
-ll min(ll a, ll b, ll c) {
-    return min(a, min(b, c));
-}
 
-int main() {
+
+int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
+    //**********
 
-    ll t;
-    cin >> t; // taking input of all the test cases
 
-    while (t--) {
-        ll b;
-        cin >> b; // number of characters in 1 test case
-        ll i;
-        ll dp1[b + 1];
-        ll dp2[b + 1];
-        dp1[0] = 1;
-        dp2[0] = dp1[0];
+    //*********
+	int n;
+    cin>>n;
 
-        for (i = 1; i <= b; ++i) {
-            char g; // taking input of all the characters one by one
-            cin >> g;
-            if (g == '+') {
-                ll e;
-                cin >> e; // taking the input of the number after + sign
-                dp1[i] = max(dp1[i - 1] + e, dp2[i - 1] + e, dp1[i - 1]);
-                dp2[i] = min(dp1[i - 1] + e, dp2[i - 1] + e, dp2[i - 1]);
-            } else if (g == '-') {
-                ll e;
-                cin >> e; // taking the input of the number after - sign
-                dp1[i] = max(dp1[i - 1] - e, dp2[i - 1] - e, dp1[i - 1]);
-                dp2[i] = min(dp1[i - 1] - e, dp2[i - 1] - e, dp2[i - 1]);
-            } else if (g == '*') {
-                ll e;
-                cin >> e; // taking the input of the number after * sign
-                dp1[i] = max(dp1[i - 1] * e, dp2[i - 1] * e, dp1[i - 1]);
-                dp2[i] = min(dp1[i - 1] * e, dp2[i - 1] * e, dp2[i - 1]);
-            } else if (g == '/') {
-                ll e;
-                cin >> e; // taking the input of the number after / sign
-                dp1[i] = max(dp1[i - 1] / e, dp2[i - 1] / e, dp1[i - 1]);
-                dp2[i] = min(dp1[i - 1] / e, dp2[i - 1] / e, dp2[i - 1]);
-            } else {
+    int k;
+    cin>>k;
 
-                // If the input is N(Means negative of the number)
-                dp1[i] = max(-1 * dp1[i - 1], -1 * dp2[i - 1], dp1[i - 1]);
-                dp2[i] = min(-1 * dp1[i - 1], -1 * dp2[i - 1], dp2[i - 1]);
-            }
-        }
-        cout << dp1[b] << "\n";
+    vector<int> arr(n+1);
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
     }
-
-    return 0;
+    cout<<solve(arr,n,k)<<endl;
+	return 0;
 }
-
+	
 
 
 /*
-input1 :
-2
+// input1 :
+5
 3
-N
-- 2
-N
-3
-- 1
-* 4
-/ 2
+1 2 3 4 8
 
 
-
-output1:
-3 (1*(-1)=>-1-2=>-3=>-(-3)=>3)
-
-4
-
-input 2 :
-
-
-output 2:
-
-
-
-
-
-input3:
-
-output3:
+// output1:
+4(len from 1 to 4)
 
 
 
---
+++ Topic_Wise_DSA/3_Two_Pointer/3_08_Numer_of_Subarray_Having_Sum_in_Range_of_L_To_R/sol.cpp
@@ -0,0 +1,39 @@
+//   Step 1 : Find number of subarrays with sum <= R
+
+// Step 2 : Find number of subarrays with sum <= L-1
+
+// return Step1 - Step2
+
+ 
+
+// sum <= R includes subarrays with sum = {0, 1, 2 , 3 4 .... R} if exists
+
+// sum <= L-1 includes subarrays with sum = {0,1,2,3,4,... L-1} if exists
+
+ 
+
+// subtract both we get sum = {L, L+1, .. R} is exists
+
+// return Ans
+
+long long solve(vector<int> &arr, int n, long long k){
+        long long sum = 0;
+        long long ans = 0;
+        int i = 0;
+        int j = 0;
+        
+        while(j < n){
+            sum += arr[j];
+            while(sum > k){
+                sum -= arr[i];
+                i++;
+            }
+            
+            ans += (j-i+1);
+            j++;
+        }
+        return ans;
+    }
+    long long countSubarray(int N,vector<int> A,long long L,long long R) {
+        return solve(A, N, R) - solve(A, N, L-1);
+    }
