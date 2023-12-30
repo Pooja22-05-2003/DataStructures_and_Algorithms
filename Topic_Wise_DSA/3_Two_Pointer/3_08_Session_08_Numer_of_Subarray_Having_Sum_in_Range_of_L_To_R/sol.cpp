583846dcce628471f1a2a11718a3ce2a25f382fe
Pooja <studentpooja1234@gmail.com>
2023-12-30 16:51:52 +0530

    File Updated

--- Topic_Wise_DSA/3_Two_Pointer/3_08_Session_08_Numer_of_Subarray_Having_Sum_in_Range_of_L_To_R/sol.cpp
+++ Topic_Wise_DSA/3_Two_Pointer/3_08_Session_08_Numer_of_Subarray_Having_Sum_in_Range_of_L_To_R/sol.cpp
@@ -1,17 +1,11 @@
-//   Step 1 : Find number of subarrays with sum <= R
+// Step 1 : Find number of subarrays with sum <= R
 
 // Step 2 : Find number of subarrays with sum <= L-1
 
 // return Step1 - Step2
 
- 
-
 // sum <= R includes subarrays with sum = {0, 1, 2 , 3 4 .... R} if exists
-
 // sum <= L-1 includes subarrays with sum = {0,1,2,3,4,... L-1} if exists
-
- 
-
 // subtract both we get sum = {L, L+1, .. R} is exists
 
 // return Ans
