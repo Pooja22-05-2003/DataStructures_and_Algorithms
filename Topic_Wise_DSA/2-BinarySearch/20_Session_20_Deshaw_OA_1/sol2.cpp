1ad57677ef9884bec0952dad98000c2676a89902
Pooja <studentpooja1234@gmail.com>
2023-12-31 12:20:27 +0530

    File Updated

--- Topic_Wise_DSA/2-BinarySearch/20_Session_20_Deshaw_OA_1/sol2.cpp
+++ Topic_Wise_DSA/2-BinarySearch/20_Session_20_Deshaw_OA_1/sol2.cpp
@@ -1,35 +1,35 @@
-// Optimized approach: https://ideone.com/SvJnz04
-/*
-Approach:
-1.Let's say we can make y groups.
-2. So, we need to maximize the value of y.
-3. So, we need to make y group and each group must be of size , then (** so (k*y) >= sum of all the elements),
-then only , we can make these groups.
-4. so ,we check for y=1 to sum of the ele
-lets say y=1 is possible.....y=2 is possible.....y=3 is possible ....but y=4 is not possible....it 
-5. It ensures that y=6,7.....will also will never be possible.
-6.So, it follow the patter of YYYYYYYNNNNNNN
-7. If we will do linear search from y=1 to (sum of all el) 
-Tc=O(sum*n) [For each iteartion check will take O(n) time]
-Sc=O(1)
+// // Optimized approach: https://ideone.com/SvJnz04
+// /*
+// Approach:
+// 1.Let's say we can make y groups.
+// 2. So, we need to maximize the value of y.
+// 3. So, we need to make y group and each group must be of size , then (** so (k*y) >= sum of all the elements),
+// then only , we can make these groups.
+// 4. so ,we check for y=1 to sum of the ele
+// lets say y=1 is possible.....y=2 is possible.....y=3 is possible ....but y=4 is not possible....it 
+// 5. It ensures that y=6,7.....will also will never be possible.
+// 6.So, it follow the patter of YYYYYYYNNNNNNN
+// 7. If we will do linear search from y=1 to (sum of all el) 
+// Tc=O(sum*n) [For each iteartion check will take O(n) time]
+// Sc=O(1)
 
-8. But by using the binary search , we can reduce the space from O(sum*n)-----> O(log(sum*n))
-*/
+// 8. But by using the binary search , we can reduce the space from O(sum*n)-----> O(log(sum*n))
+// */
 
 
-//Tc=O(log(sum*n))
-// sc=O(1)
+// //Tc=O(log(sum*n))
+// // sc=O(1)
 
-/*
-Check function working.
+// /*
+// Check function working.
 
-check function is checking whther it is possible to make y groups for particular k value.
--> Here y=maximum number of portfolio
--> This is only possible when y*k elements are atleast present in the array.
--> So for every index ....if(number of el is greater than y..than we can pick those elements and move forward).
--> But if less element are present at index i than required, then we pick those elements and fill our reaming requirement from next index.
--> After itearting all index, if we were able to fill the requirement which is target(we will return true) else false.
-*/
+// check function is checking whther it is possible to make y groups for particular k value.
+// -> Here y=maximum number of portfolio
+// -> This is only possible when y*k elements are atleast present in the array.
+// -> So for every index ....if(number of el is greater than y..than we can pick those elements and move forward).
+// -> But if less element are present at index i than required, then we pick those elements and fill our reaming requirement from next index.
+// -> After itearting all index, if we were able to fill the requirement which is target(we will return true) else false.
+// */
 #include<bits/stdc++.h>
 using namespace std;
 bool check( int y, int k,vector<int>arr,int n){
