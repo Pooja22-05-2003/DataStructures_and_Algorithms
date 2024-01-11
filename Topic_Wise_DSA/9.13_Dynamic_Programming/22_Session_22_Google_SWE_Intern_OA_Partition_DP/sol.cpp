e78e3d5a0fe196b0cb18a89f32cc9ee6c7dc5266
Pooja <studentpooja1234@gmail.com>
2024-01-11 23:44:52 +0530

    File Updated

--- Topic_Wise_DSA/9.13_Dynamic_Programming/22_Session_22_Google_SWE_Intern_OA_Partition_DP/sol.cpp
+++ Topic_Wise_DSA/9.13_Dynamic_Programming/22_Session_22_Google_SWE_Intern_OA_Partition_DP/sol.cpp
@@ -1,3 +1,30 @@
+/*
+Problem in 1 Line : Array is of size n, and u need to divide the array into subaarays , such that its cost would be minimum.
+and the subarray is of length 1 , then its cost would be a[i]
+and if the subarray is of length 2 or more, then its cost would be b[i]+b[i+1]......b[j] 
+
+Observation :
+1. So this is a DP problem and you can use the previously computer answer for the current answers.
+2. So try to follow the general rules of the dp.
+3. Try to calcualte dp[1]...then dp[2]...then d[3] and till now try to come with some general formula.
+4. So, Example Input : 
+a-> [ 3 5 2  1 9]
+b-> [ 1 1 10 5 3] 
+      1 2 3  4 5
+
+dp[0]=0
+dp[1]= a[1]
+dp[2]=max(a[1]+a[2],(b[1]+b[2]))
+dp[3]=max(dp[i-1]+a[3] , dp[i-2]+b[2]+b[3])
+
+general formula dp[i]=max(dp[i-1]+a[i] , dp[i-2]+b[i-1]+b[i])
+
+5. Use the general formula and compute the dp[n] and that would be the answer.
+
+
+
+*/
+
 // TC= O(N2)
 // SC= O(N)
 #include<bits/stdc++.h>
