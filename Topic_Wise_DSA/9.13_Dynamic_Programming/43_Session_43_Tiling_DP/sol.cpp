9977ab8f46110822cb01e4e642fd0bdb3208ee18
Pooja <studentpooja1234@gmail.com>
2024-01-19 19:58:25 +0530

    File Updated

--- Topic_Wise_DSA/9.13_Dynamic_Programming/43_Session_43_Tiling_DP/sol.cpp
+++ Topic_Wise_DSA/9.13_Dynamic_Programming/43_Session_43_Tiling_DP/sol.cpp
@@ -1,5 +1,15 @@
+/*
+1. See the gfg solution for the better clarifaction the solution for n=3.
+
+2. for n=1, ans=1 (place vertically)
+   for n=2, ans=2 (place both vertically and place both horizontally)
+   for n=3, ans=3 (for n=1 and n=3 add the third till horizontally with all the possible ways for n=2,1)
+3. So the general formula is (dp[i]=dp[i-1]+dp[i-2])
+4. return dp[n] as the final ans.
+*/
+
 // TC=O(N)
-// SC=O(N*3)
+// SC=O(N)
 
 #include<bits/stdc++.h>
 using namespace std;
