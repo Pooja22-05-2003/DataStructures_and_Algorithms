87e0976c4a05e4e11414696e5b3a7c3bbaff5e28
Pooja <studentpooja1234@gmail.com>
2024-01-27 20:55:22 +0530

    File Updated

--- Topic_Wise_DSA/9.19_Priority_Queue/02_(27_Jan_2024)_Atlassian_OA_3_Priority_Queue/sol.cpp
+++ Topic_Wise_DSA/9.19_Priority_Queue/02_(27_Jan_2024)_Atlassian_OA_3_Priority_Queue/sol.cpp
@@ -1,6 +1,31 @@
+/*
+Observations:
+
+1.Brilliant observation :- Any character can go anywhere.  
+
+2.Brilliant observation :- It is now easy to solve ; just make all the places empty and then fill those empty spots by the characters you have in such a manner that maximum strings become palindromes!
+
+3.Brilliant observation :- Sort all the empty places according to their size from small to large! :-> And then start filling the smallest empty place because it has the highest chances of being a plaindrome! 
+
+4.Brilliant observation :- Lets handle the empty space of odd length and even length separately 
+
+5.Brilliant observation :- To fill even length space :-> You try all characters with even frequency and try the largest ones first greedily -> In most cases it will work -> if you exhaust all the even characters -> you will start using the odd characters :-> take them all but dont use their odd guy thats it 
+
+6.Brilliant observation :- Odd length? :p Ignore the middle and follow the same algorithm to fill the rest of the places as their length is even.  With which char should you try to fill the middle guy ? -> Odd char frequency just take 1 from him; if it works good. If its not available take 1 frequency from any even guy 
+
+7.Last observation :-> Proof of the greedy theorem :-> Lets say you are trying to make the 4th string palindrome; and you fail do it then you stop your algorithm and declare 3 as your answer
+
+Who is in this world told you that 5th string palindrome making wont be possible? 
+
+Lets prove it and analyse:- > Watch video for it :)  
+
+*/
+
 // TC=O()
 // SC=O()
 
+
+// Ideone Link: https://ideone.com/WxJt5Q
 #include <bits/stdc++.h>
 
 using namespace std;
