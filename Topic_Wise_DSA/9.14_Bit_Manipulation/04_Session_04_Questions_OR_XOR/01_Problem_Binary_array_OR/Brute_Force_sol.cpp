387c4c68ba6409a456ab84e465425cafb48c5e4a
Pooja <studentpooja1234@gmail.com>
2024-01-31 11:20:24 +0530

    File Updated

---
+++ Topic_Wise_DSA/9.14_Bit_Manipulation/04_Session_04_Questions_OR_XOR/01_Problem_Binary_array_OR/Brute_Force_sol.cpp
@@ -0,0 +1,66 @@
+// TC=O(N2)
+// SC=O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int n ,vector<int>arr){
+    int ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=i+1;j<=n;j++)
+        {
+            int OR_operation=arr[i] | arr[j];
+            ans=ans+OR_operation;
+
+        }
+    }
+
+    return ans;
+    
+}  
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    //**********
+     
+    // Enter the decimal number.
+    int n;
+    cin>>n;
+
+    vector<int>arr(n+1);
+
+    for(int i=1;i<=n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<solve(n,arr)<<endl;
+	return 0;
+}
+	
+
+
+/*
+// input1 :
+8
+0 1 1 0 0 0 1 1
+
+// output1:
+22
+
+
+// input 2 :
+
+
+// output 2:
+
+
+
+
+
+
+
+*/
+
