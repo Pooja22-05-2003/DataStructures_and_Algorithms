0e980f5f270a48905ecf681a08ee060202fa0cf6
Pooja <studentpooja1234@gmail.com>
2024-02-01 10:21:47 +0530

    File Updated

--- Topic_Wise_DSA/9.14_Bit_Manipulation/11_Session_11_Atlassian_OA/sol.cpp
+++ Topic_Wise_DSA/9.14_Bit_Manipulation/11_Session_11_Atlassian_OA/sol.cpp
@@ -1,5 +1,30 @@
-// TC=O(N)
-// SC=O(N)
+/*
+
+
+Approach : 
+
+First you pick up all the numbers whose 0th bit is 1 ; whatever their size it can be your answer because the final bitwise AND!=0 ; as the the 0th bit of end result will always be 1 -> g1
+
+-> You can do same as above for the 1st bit -> g2 
+
+-> You can do same as above for the 2nd bit -> g3 
+.
+.
+.
+.
+.
+.
+.
+-> You can do the same as above for the 30th bit -> g30
+
+2. For each number check for the ith bit. If it is not 0, then that numeber will contribute to the subsequence.
+3. For each bit fronm 0 to 30, at each bit, count the number of set bits.
+max count of bit will contribute to the maximum subsequence.
+
+*/
+
+// TC=O(N*30)
+// SC=O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
