39b42e2d3ccd99a4b5191bd5b4716132417846a3
Pooja <studentpooja1234@gmail.com>
2024-03-16 16:24:50 +0530

    File Updated

---
+++ CORE_SUBJECTS/1_OOPS/07_this_pointer/07_01.cpp
@@ -0,0 +1,11 @@
+/*
+this pointer:
+
+1. this is a keyword
+2. this is a local object pointer in every instance member function 
+containing address of the caller object.
+3. this pointer can not be modified.
+4. It is used to refer caller object in member function.
+
+
+*/
