fc139208d1bb79af0fd035c1a7e6986604b95710
Pooja <studentpooja1234@gmail.com>
2024-03-26 18:10:13 +0530

    File Updated

---
+++ Topic_Wise_DSA/9.15_Greedy/07_Mice_and_Cheese_Lc_211/sol.cpp
@@ -0,0 +1,41 @@
+/*
+
+// TC=O(nlogn)
+// SC=O(n)
+
+
+Its mentioned in the question that mice 1 will need to select 'k' cheese and mice 2 can select the remaining cheese. We need to select the 'k' cheese in such a way that the reward will be maximum.
+I am using a priority queue <difference between reward1[i] and reward2[i], reward1[i], reward2[i]>
+By default the heap will be created based on the first element. It can be said that whenever we select reward1 'k' times, the one with maximum difference will be optimal.
+So we select the first 'k' elements from reward1 and then the remaining elements from reward2 will give us the optimal answer.
+*/
+
+class Solution {
+public:
+    int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
+        int n=reward1.size();
+        priority_queue<vector<int>>pq;
+        int ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            int diff=reward1[i]-reward2[i];
+            pq.push({diff,reward1[i],reward2[i]});
+        }
+        
+        while(!pq.empty() && k--)
+        {
+            ans+=pq.top()[1];
+            pq.pop();
+        }
+        
+        while(!pq.empty())
+        {
+            ans+=pq.top()[2];
+            pq.pop();
+        }
+        
+        return ans;
+        
+    }
+};
