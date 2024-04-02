bbb9db3b79f511bd0434ada4f89b75b9fd634916
Pooja <studentpooja1234@gmail.com>
2024-04-02 20:51:34 +0530

    File Updated

---
+++ Topic_Wise_DSA/9.13_Dynamic_Programming/001_DP_WITH_CODEWITH_MIK_and_Striver_Sheet_Que/04_DP_On_Strings/06_Delete_Operation_for_Two_String/Space_Optimization.cpp
@@ -0,0 +1,53 @@
+/*
+
+//✨✨ Variation of LCS
+// Space Optimization=> ✨✨Do the right shift in the tabulation
+// TC=O(len1*len2)
+// SC=O(len2)
+
+*/
+class Solution {
+public:
+        int longestCommonSubsequence(string text1, string text2) {
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<int>prev(n2+1,0);
+        vector<int>curr(n2+1,0);
+        
+        
+        // right shift by 1 index
+       
+        
+        for(int ind1=1;ind1<=n1;ind1++)
+        {
+            for(int ind2=1;ind2<=n2;ind2++)
+            {
+                 int take=0;
+                 if(text1[ind1-1]==text2[ind2-1])  // index-1 we are doing , becuase we considered the right shift
+                 {
+                     take=1+prev[ind2-1];
+                 }
+        
+                 // not take 
+                 int  nottake=max(prev[ind2],curr[ ind2-1] );
+        
+                 curr[ind2]=max(nottake, take);
+            }
+            prev=curr;
+        }
+            
+        return prev[n2];
+    }
+   
+   
+    int minDistance(string s, string t) {
+        int len=longestCommonSubsequence(s,t);
+        
+        int l1=s.size();
+        int cnt1=l1-len;
+        int  l2=t.size();
+        int cnt2=l2-len;
+        
+        return (cnt1+cnt2);
+    }
+};
